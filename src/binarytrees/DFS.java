d148eb86d2b1eaef22dd6c2225d095636f1545ea
Muhammad Zohaib Mohsin <zohaibmohsin30@gmail.com>
2024-04-13 22:57:58 +0500

    Trees completed

---
+++ src/binarytrees/DFS.java
@@ -0,0 +1,20 @@
+package binarytrees;
+
+import java.util.LinkedList;
+
+public class DFS {
+
+    public static void main(String[] args) {
+        //This class is designed to work on Depth First Search using stacks
+
+//        Stack<TreeNode> top = new LinkedList<TreeNode>();
+//        stack.push(root);
+//
+//        while(!top.isEmpty()){
+//            TreeNode work = top.pop();
+//
+//            stack.push(root.right);
+//            stack.push(root.left);
+        }
+    }
+//}
